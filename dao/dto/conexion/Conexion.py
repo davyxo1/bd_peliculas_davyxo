d3e4a6be04139a9a8fbd15635249e9f1c05de85d
David Bustamante <davidbq0003@gmail.com>
2025-06-30 13:48:57 -0400

    1.5

--- dao/dto/conexion/Conexion.py
+++ dao/dto/conexion/Conexion.py
@@ -1,9 +1,14 @@
 import mysql.connector
 
-def obtener_conexion():
-    return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='',
-        database='bd_peliculas'
-    )
+def conectar():
+    try:
+        conexion = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='',
+            database='bd_peliculas'
+        )
+        return conexion
+    except Exception as e:
+        print(f"Error al conectar: {e}")
+        return None

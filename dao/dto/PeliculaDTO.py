2ce4baac5d0d3072c64520ba9cc3c6057c7209c2
David Bustamante <davidbq0003@gmail.com>
2025-06-30 12:26:44 -0400

    1.2

--- dao/dto/PeliculaDTO.py
+++ dao/dto/PeliculaDTO.py
@@ -1,9 +1,12 @@
 class PeliculaDTO:
-    def __init__(self, id_pelicula=None, titulo="", duracion=0, fecha_de_estreno=None, genero=0, idioma=0, director=""):
+    def __init__(self, id_pelicula, titulo, duracion, fecha_estreno, genero, idioma, director):
         self.id_pelicula = id_pelicula
         self.titulo = titulo
         self.duracion = duracion
-        self.fecha_de_estreno = fecha_de_estreno
+        self.fecha_estreno = fecha_estreno
         self.genero = genero
         self.idioma = idioma
         self.director = director
+
+    def __str__(self):
+        return f"{self.id_pelicula} - {self.titulo} ({self.duracion} min) - Dirigida por {self.director}"
